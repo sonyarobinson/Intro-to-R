c66a4e36d170170af71b120d8bb1cd7498076045
Sonya Robinson <sonyaclarerobinson@gmail.com>
2020-08-23 20:16:32 -0700

    Revert "Added My Name"
    This reverts commit c5b83ceec2a36f0c724fbb97d03d514b0ccc23aa.

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,5 @@
-# R script to accompany Intro to R
-# Copy for Sonya Robinson 2020
+# R script to accopany Intro to R
+# Copy for RStudent 2019
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
